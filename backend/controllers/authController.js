967510d3762b8d079b68a1ffa80bd90a541c6a73
sanchi-t <98596642+sanchi-t@users.noreply.github.com>
2022-11-19 20:27:27 +0530

    Creating auth for signup and installing dependecies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,31 @@
+const user = require('../models/users');
+
+
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+  
+    // duplicate email error
+    if (err.code === 11000) {
+      errors.email = 'that email is already registered';
+      return errors;
+    }
+  
+    // validation errors
+    if (err.message.includes('Login_Detail validation failed')) {
+      // console.log(err);
+      Object.values(err.errors).forEach(({ properties }) => {
+        // console.log(val);
+        // console.log(properties);
+        errors[properties.path] = properties.message;
+      });
+    }
+  
+    return errors;
+  }
+
+
+
 module.exports.signup_get=(req,res)=>{
     res.send('signup get')
 }
@@ -6,10 +34,22 @@ module.exports.login_get=(req,res)=>{
     res.send('login get')
 }
 
-module.exports.signup_post=(req,res)=>{
-    res.send('signup post')
-}
+module.exports.signup_post = async (req, res) => {
+    const { email, password } = req.body;
+    
+  
+    try {
+      const user1 = await user.create({ email, password });
+      res.status(201).json(user1);
+    }
+    catch(err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
+   
+   
+  }
 
 module.exports.login_post=(req,res)=>{
-    res.send('signup post')
+    res.send('login post')
 }
