b8d860bc14136df09750ff0f564988313c6b9602
sanchi-t <98596642+sanchi-t@users.noreply.github.com>
2022-11-19 11:08:11 +0530

    Created simple express server

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,18 @@
-console.log("hello");
-console.log('hey');
+const express = require('express');
+const mongoose = require ("mongoose");
+const app = express();
+
+
+// app.use (express.static('public'));
+
+app.set('view engine', 'ejs');
+
+const dbURI= 'mongodb+srv://Nishant8917:Dungeon@8917@cluster0.qvrpeky.mongodb.net/?retryWrites=true&w=majority';
+mongoose . connect (dbURI, { useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex: true })
+.then ((result) => app.listen (3000))
+.catch((err) => console . log(err));
+
+app.get('/',(req, res) => res.render("home"));
+
+
+app.listen(4000);
